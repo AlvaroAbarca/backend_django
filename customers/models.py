9bee886b778f987e149f5b025642941510b4b2fe
Alvaro Abarca <dead.time.aa@gmail.com>
2021-02-15 23:35:20 -0300

    Account

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class Account(AbstractUser):
+
+    class Meta:
+        verbose_name = ("Account")
+        verbose_name_plural = ("Accounts")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Account_detail", kwargs={"pk": self.pk})
